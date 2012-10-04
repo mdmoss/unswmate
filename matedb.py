252a4bc5efa2b40e818c2d0618dab7e33e164027
Matt <mdm@cse.unsw.edu.au>
2012-10-04 23:18:42 +1000

    Add more functions to matedb (coding, 20)

--- matedb.py
+++ matedb.py
@@ -7,6 +7,23 @@ c = conn.cursor()
 def get_all_mates(user):
     t = (user,)
     mates = [];
-    for mate in c.execute('SELECT * FROM mates WHERE user=?', t):
-        mates.append(mate[2])
+    for mate in c.execute('SELECT mate FROM mates WHERE user=?', t):
+        mates.append(mate[0])
     return mates
+    
+def get_all_courses(user):
+    t = (user,)
+    courses = [];
+    for courses in c.execute('SELECT course FROM courses WHERE user=?', t):
+        courses.append(course[0])
+    return courses
+    
+def get_data(user, field):
+    t = (user,)
+    # I'm a bad man
+    query = 'SELECT %s FROM users WHERE username=?' % field
+    c.execute(query, t)
+    if c.rowcount > 0:
+        return c.fetchone()[0]
+    else:
+        return ''
