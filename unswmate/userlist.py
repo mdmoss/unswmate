960a41e211bddcac8d81e37be4f99eda04032977
Matt <mdm@cse.unsw.edu.au>
2012-10-16 16:21:50 +1100

    Tweak matelist and CSS

--- unswmate/userlist.py
+++ unswmate/userlist.py
@@ -4,9 +4,16 @@ import matedb as db
 import images as images
 
 def format(users):
-    result = "";
+    result = '';
 
-    for user in users:
-        result += '<div class="span2"><a href="?who=' + user + '">' + db.get_data(user, 'name') + images.get_profile_picture(user) + "</a></div>\n"
-        
+    for i in xrange (0, len(users), 6):
+        group = users[i:i+6]
+
+        result += '<div class="row">'
+
+        for user in group:
+            result += '<div class="span2"><a href="?who=' + user + '"><p>' + db.get_data(user, 'name') + '</p>' + images.get_profile_picture(user) + "</a></div>\n"
+
+        result += '</div>'
+       
     return result
